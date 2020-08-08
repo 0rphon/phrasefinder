00964f47d45b095ae049ba90cb0aa9191af29328
0rphon <59403052+0rphon@users.noreply.github.com>
2020-08-07 22:42:00 -0700

    improved output

--- phrasefinder.py
+++ phrasefinder.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sys import argv
 from os import listdir, path as pathlib
 
+
 def get_args():
     if "--help" in argv:
         print("Usage: python phrasefinder.py [name of text folder] [name of xlsx file]")
@@ -14,11 +15,15 @@ def get_args():
         exit(2)
     else:
         return (argv[1],argv[2])
-    
+
 
 def get_files(path):
     if pathlib.exists(path):
-        return ["/".join((path,f)) for f in listdir(path) if f.endswith(".txt")]
+        files = ["/".join((path,f)) for f in listdir(path) if f.endswith(".txt")]
+        file_data = []
+        for name in files:
+            file_data.append((name,open(name, "r").read()))
+        return file_data
     else:
         print("Error: cannot find text file directory")
         exit(3)
@@ -32,29 +37,62 @@ def get_phrases(doc):
         exit(4)
 
 
-def main():
-    (txt_dir, xlsx_path) =  get_args()
-    phrases = get_phrases(xlsx_path)
-    files = get_files(txt_dir)
-    output = open("output.txt","w")
-
-    for name in files:
-        text = open(name, "r").read()
-        for phrase in phrases:
+def find_phrases(phrases, files):
+    html_tags = ["<div>", "</div>", "<br>", "<b>", "</b>", "<u>", "</u>", "<li>", "</li>", "<i>", "</i>", "<ol>", "</ol>"]
+    found = {}
+    for phrase in phrases:
+        found[phrase] = []
+        for (name,text) in files:
             split = text.partition(phrase)
             while len(split[1]) == len(phrase):
-                context = (
-                            (" ".join(split[0].split(" ")[-10:])).replace("\n","    ") + 
-                            split[1] + 
-                            (" ".join(split[2].split(" ")[:10])).replace("\n","    ")
-                )
-                result = name+"      "+context
-                print(result)
-                output.write(result+"\n")
+                context = "{} {} {}".format(
+                        (" ".join(split[0].split(" ")[-10:])), 
+                        split[1], 
+                        (" ".join(split[2].split(" ")[:10]))
+                    ).replace("\n", " ")
+                for x in html_tags: context = context.replace(x, "")
+                found[phrase].append((name.split("/")[-1], context))
                 split = split[2].partition(phrase)
+    return found
 
-    print("results saved to output.txt")
+
+def display_and_save(found, longest_filename):
+    output = open("output.txt","w")
+    for (phrase, instance) in found.items(): 
+        if len(instance) != 0:
+            out = '\n"{}"'.format(phrase)
+            print(out)
+            output.write(out+"\n")
+            for (filename, context) in instance:
+                out = "    {n:{l}}    {c}".format(n=filename, l=longest_filename, c=context)
+                print(out)
+                output.write(out+"\n")
+            print("\n")
+            output.write("\n")
+        else:
+            out = '"{}" not found'.format(phrase)
+            print(out)
+            output.write(out+"\n")
     output.close()
+
+
+def main():
+    (txt_dir, xlsx_path) =  get_args()
+
+    phrases = get_phrases(xlsx_path)
+    files = get_files(txt_dir)
+    longest_filename = 0
+    for (name,_) in files: 
+        legnth = len(name.split("/")[-1])
+        if legnth > longest_filename: longest_filename = legnth
+
+    found = find_phrases(phrases, files)
+    display_and_save(found, longest_filename)
+
+    total = 0
+    for (_,data) in found.items():
+        total+=len(data)
+    print("\n%d results saved to output.txt"%total)
     exit(0)
 
 
